6295560135c24a6a4d86da14985b4dd36b6cd32a
thegamerx1 <34986564+thegamerx1@users.noreply.github.com>
2023-06-07 23:09:53 +0200

    Setup bucket

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "thegamerx1/scoop_bucket:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
